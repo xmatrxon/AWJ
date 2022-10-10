505c1f7c5eeeb23d3c084118070d64a431260ff0
Mateusz Będkowski <mateusz.bedkowski@op.pl>
2022-10-10 20:12:24 +0200

    git init

---
+++ kred_01/src/main/java/com/jsfcourse/calc/Kalkulator.java
@@ -0,0 +1,76 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+public class Kalkulator {
+	private String x;
+	private String y;
+	private String z;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	public String getZ() {
+		return z;
+	}
+
+	public void setZ(String z) {
+		this.z = z;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double x = Double.parseDouble(this.x);
+			double y = Double.parseDouble(this.y);
+			double z = Double.parseDouble(this.z);
+
+			result = x*(z/100)*y;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+}
